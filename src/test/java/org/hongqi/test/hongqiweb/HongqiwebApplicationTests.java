d9c3af8038958bce9f7ce1e3c32ee60514c17806
senpec <15900497661@163.com>
2023-08-13 02:59:22 +0800

    first

---
+++ src/test/java/org/hongqi/test/hongqiweb/HongqiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hongqi.test.hongqiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongqiwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
